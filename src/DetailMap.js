bf4702e9456574896d1572b4270a059745f3f286
Vladyslav Cholak <vchol@softserveinc.com>
2020-12-06 02:36:54 +0200

    Prediction on button

--- src/DetailMap.js
+++ src/DetailMap.js
@@ -245,7 +245,7 @@ class App extends Component {
           <p><b>Загалом випадків </b><span style={{ color: '#A90000' }}>{this.state.dataTotal.total_cases}</span></p>
           <p><b>Нові випадки </b><span style={{ color: '#A90000' }}>{this.state.dataTotal.new_cases}</span></p>
           <p><b>Загалом одужали </b><span style={{ color: '#28a745' }}>{this.state.dataTotal.total_recovered}</span></p>
-          <p><b>Загалом вмерли </b><span>{this.state.dataTotal.total_deaths}</span></p>
+          <p><b>Загалом померли </b><span>{this.state.dataTotal.total_deaths}</span></p>
           <p><b>Нові померлі </b><span>{this.state.dataTotal.new_deaths}</span></p>
           <div className="select">
             <div id="select">
